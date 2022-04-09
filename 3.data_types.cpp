83ed7322a1cb2fc65840ce36ec12564582d3194b
Ayush-Juyal <78803826+Ayush-Juyal@users.noreply.github.com>
2022-04-09 20:39:48 +0530

    Create 3.data_types.cpp

---
+++ 3.data_types.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+scanf("%d %ld %c %f %lf",&a,&b,&c,&d,&e);
+
+printf("%d\n%ld\n%c\n%f\n%lf\n",a,b,c,d,e); 
+/*printf("%d\n",a);
+printf("%ld\n",b);
+printf("%c\n",c);
+printf("%f\n",d);
+printf("lf\n",e);*/
+
+
+    
+    /*cin>>a>>b>>c>>d>>e;
+    cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<d<<"\n"<<e;*/
+    // Complete the code.
+    return 0;
+}
